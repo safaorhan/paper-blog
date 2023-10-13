63a0cdb193097d40d923699f72781932f6158ad8
safa <ahmetsafaorhan@gmail.com>
2023-10-13 21:32:55 +0300

    Merge app with blog.

---
+++ db.js
@@ -0,0 +1,86 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+
+const id = new URLSearchParams(window.location.search).get("id") || navigateToWarningDoc();
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC8eHruJQ8K5gcHlXWq6sSfIbdRGnoSU_c",
+    authDomain: "paper-9a0b5.firebaseapp.com",
+    projectId: "paper-9a0b5",
+    storageBucket: "paper-9a0b5.appspot.com",
+    messagingSenderId: "596663898807",
+    appId: "1:596663898807:web:07539d69005dbab4fc3332"
+}
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+const postRef = doc(db, "posts", id);
+
+window.auth = auth
+
+const dom = {
+    date: document.querySelector("#date"),
+    title: document.querySelector("#title"),
+    content: document.querySelector("#content")
+}
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        startPaper()
+    } else {
+        showSigninPopup()
+    }
+});
+
+async function startPaper() {
+    await loadPost()
+    startAutoSave()
+}
+
+function navigateToWarningDoc() {
+    window.location = "?id=warning";
+}
+
+function navigateToBlog() {
+    window.location = "/"
+}
+
+async function showSigninPopup() {
+    try {
+        await signInWithPopup(auth, provider);
+    } catch (ex) {
+        console.error(ex)
+        navigateToBlog()
+    }
+}
+
+function startAutoSave() {
+    const observer = new MutationObserver(async () => {
+        await savePost()
+    });
+
+    observer.observe(dom.date, { subtree: true, childList: true, characterData: true });
+    observer.observe(dom.title, { subtree: true, childList: true, characterData: true });
+    observer.observe(dom.content, { subtree: true, childList: true, characterData: true });
+}
+
+async function loadPost() {
+    const post = (await getDoc(postRef)).data()
+
+    if (post) {
+        dom.date.innerHTML = post.date
+        dom.title.innerHTML = post.title
+        dom.content.innerHTML = post.content
+    }
+}
+
+async function savePost() {
+    await setDoc(postRef, {
+        date: dom.date.innerHTML,
+        title: dom.title.innerHTML,
+        content: dom.content.innerHTML
+    });
+}
---
+++ paper.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    showTodaysDate()
+    
+    document.querySelectorAll(".editable").forEach((element) => {
+        element.setAttribute("contenteditable", true)
+    })
+
+    const title = document.querySelector(".title")
+
+    const content = document.querySelector(".content")
+
+    title.focus()
+
+    title.addEventListener("keydown", (event) => {
+        if (event.isComposing || event.keyCode === 229) {
+            return;
+        }
+    
+        // Enter || Down Arrow
+        if ((event.keyCode === 13 || event.keyCode === 40) && !event.shiftKey) {
+            content.focus()
+            event.preventDefault()
+        }
+
+        // Backspace
+        if (event.keyCode === 8 && title.children[0].innerHTML == "<br>") {
+            event.preventDefault()
+        }
+    });
+
+    content.addEventListener("keydown", (event) => {
+        if (event.isComposing || event.keyCode === 229) {
+            return;
+        }
+        
+        // Up Arrow
+        if (event.keyCode === 38) {
+            const firstP = document.querySelector(".content > p:first-child")
+            const focusNode = document.getSelection().focusNode
+            const focusedP = focusNode.nodeName == "#text" ? focusNode.parentElement : focusNode
+
+            if (firstP.isEqualNode(focusedP)) {
+                title.focus()
+                event.preventDefault()
+            }
+        }
+
+        // Backspace
+        if (event.keyCode === 8) {
+            if (content.children.length == 1 && content.children[0].innerHTML == "<br>") {
+                event.preventDefault()
+            }
+        }
+    });
+});
+
+function showTodaysDate() {
+    const today = new Date()
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+
+    document.querySelector("#date").innerHTML = today.toLocaleDateString('en-US', options)
+}
