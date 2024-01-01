fb135764cfdfa6893dc1e4401bbb64edb65d4531
safa <ahmetsafaorhan@gmail.com>
2024-01-01 10:41:58 +0300

    Address PR comments.

--- js/shortcutManager.js
+++ js/shortcutManager.js
@@ -5,18 +5,22 @@ export async function handleKeyDown(event) {
 
     if (event.code == "KeyB" && event.ctrlKey && event.altKey) {
         editor.makeSelectionBold();
+        event.preventDefault();
     }
 
     if (event.code == "KeyI" && event.ctrlKey && event.altKey) {
         editor.makeSelectionItalic();
+        event.preventDefault();
     }
 
     if (event.code == "KeyU" && event.ctrlKey && event.altKey) {
         editor.makeSelectionUnderline();
+        event.preventDefault();
     }
 
     if (event.code == "ArrowUp" && event.ctrlKey && event.altKey) {
         editor.makeSelectionSuperscript();
+        event.preventDefault();
     }
 
     if (event.code == "Digit1" && event.ctrlKey && event.altKey) {
@@ -31,9 +35,11 @@ export async function handleKeyDown(event) {
 
     if (event.code == "KeyC" && event.ctrlKey && event.altKey) {
         editor.makeCodeBlock();
+        event.preventDefault();
     }
 
-    if (event.keyCode == "KeyD" && event.ctrlKey && event.altKey) {
+    if (event.code == "KeyD" && event.ctrlKey && event.altKey) {
         await publishPost();
+        event.preventDefault();
     }
 }
